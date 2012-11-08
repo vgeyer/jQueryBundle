f3abfce0a0260b34f0794fecc2575c4c07ec2952
Victor J. C. Geyer <victor.geyer@googlemail.com>
2012-11-08 23:19:05 +0100

    added jQuery v1.8.2

---
+++ lxjQueryBundle.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of the jQueryBundle
+ *
+ * (c) Victor J. C. Geyer <victorgeyer@ciscaja.com>
+ */
+
+namespace lx\jQueryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class lxjQueryBundle extends Bundle
+{
+}
